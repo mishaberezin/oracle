b73cfd3c5a50779e54592ca74b48e9475ada938b
Misha Berezin <mishaberezin@gmail.com>
2019-03-11 19:25:19 +0300

    initial

---
+++ www/index.js
@@ -0,0 +1,14 @@
+({
+  offline: false,
+  data: {
+    revision: 123,
+    prophecies: {}
+  },
+  user: {
+    fake: true,
+    login: "123",
+    password: "password",
+    photo: "",
+    name: ""
+  }
+});
